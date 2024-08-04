687449f35b0aa6d3bf1ec97e38b061a117e3da13
Hamad Ur Rehman <ceerhamad@gmail.com>
2024-08-04 13:41:49 +0500

    collecting pictures to the website

---
+++ src/components/FoodCategory.js
@@ -0,0 +1,25 @@
+import React from "react";
+
+function FoodCategory(){
+    return (
+        <div className="food-category">
+            <h2>Explore Foods</h2>
+            <div className="categories">
+                <div className="category">
+                <img src={`${process.env.PUBLIC_URL}/images/pizza.jpg`} alt="Pizza" />
+                <h3>Pizza</h3>
+                </div>
+                <div className="category">
+          <img src={`${process.env.PUBLIC_URL}/images/burger.jpg`} alt="Burger" />
+          <h3>Burger</h3>
+        </div>
+        <div className="category">
+          <img src={`${process.env.PUBLIC_URL}/images/momo.jpg`} alt="Momo" />
+          <h3>Momo</h3>
+     </div>
+    </div>
+   </div>
+    );
+}
+
+export default FoodCategory;
