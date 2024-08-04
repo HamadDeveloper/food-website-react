0c3fc3c922fd71345073e4d78e6ee2b77e4bd5ca
Hamad Ur Rehman <ceerhamad@gmail.com>
2024-08-04 10:48:32 +0500

    adding the component Header,SearchBar

---
+++ src/components/SearchBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+
+function SearchBar(){
+    return (
+        <div className="search-bar">
+            <input type="text" placeholder="Search for Food ..."></input>
+            <button>Search</button>
+        </div>
+    );
+}
+ export default SearchBar;
